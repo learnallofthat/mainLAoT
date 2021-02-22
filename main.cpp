ea352048128ed7f9257e7b2abce8dec56c31648d
admin <ibogdanovdec@gmail.com>
2021-02-22 16:13:21 +0300

    New user in project. Test 0.1

--- main.cpp
+++ main.cpp
@@ -5,8 +5,14 @@ void helloFromNewBranch()
     std::cout << "Hello github from new branch.\n";
 }
 
+void helloFromAdmin()
+{
+    std::cout << "Hi, I am Admin.\n";
+}
+
 int main(int argc, char **argv)
 {
     helloFromNewBranch();
+    helloFromAdmin();
     return 0;
 }
